73061b4a10563fa8b816a65bef6f86b913e5d2df
Leonardo <leonardomaia166@gmail.com>
2026-05-24 17:37:03 -0300

    quarto commit do JS

--- main.js
+++ main.js
@@ -39,7 +39,15 @@ function receberPorcentagemBotao(evento){
             botao.classList.add("botao-ativo")
         } 
     })
- 
-    porcentagem = parseFloat(evento.target.value) / 100
+
+    if(evento.target.value !== ""){
+        porcentagem = parseFloat(evento.target.value) / 100
+    }else {
+        porcentagem = 0
+    }
 }
- 
+    
+    console.log(porcentagem)
+
+const gorjetaInput = document.querySelector("#outra")
+ gorjetaInput.addEventListener("input", receberPorcentagemBotao)
