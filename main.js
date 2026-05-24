ded1836edda4f67859df83f8fac78460f06aaa54
Leonardo <leonardomaia166@gmail.com>
2026-05-24 17:26:13 -0300

    nono commit do CSS e tercero commit do JS

--- main.js
+++ main.js
@@ -1,38 +1,45 @@
 let conta = 0
 let pessoas = 0
-
-const containput = document.querySelector(".containput")
-   containput.addEventListener("input", )
-
+let porcentagem = 0 
+ 
+const containput = document.querySelector("#conta") 
+   containput.addEventListener("input", receberValorconta) 
+ 
 function receberValorconta(evento){
     conta = Number(evento.target.value)
 }
-
+ 
 const pessoasinput = document.querySelector("#pessoas")
-pessoasinput.addEventListener("input", )
-
+pessoasinput.addEventListener("input", receberQuantidadePessoas) 
+ 
 function receberQuantidadePessoas(evento){
  const paragrafoErro = document.querySelector(".pessoas #erro")
-
-    if(evento.target.value === "0"){
-        document.querySelector(".pessoas #erro")
-        const divErro = document.querySelector(".pessoas .input-box")
+ const divErro = document.querySelector(".pessoas .input-box") 
+ 
+if(evento.target.value === "0"){
         paragrafoErro.style.display = "block"
-        divErro.setAttribute("id", "erro-div")
-    } else {
+        divErro.classList.add("erro-ativo")
+} else {
         paragrafoErro.style.display = "none"
-        divErro.removeAttribute("id", "")
+        divErro.classList.remove("erro-ativo")
         pessoas = Number(evento.target.value)
     }
 }
-
+ 
 const botoesgorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesgorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagem)
+    botao.addEventListener("click", receberPorcentagemBotao) 
 })
-
-function receberPorcentagem(){
-    console.log(evento.target.value)
-    porcentagem = Number(evento.target.value) / 100
-    console.log(porcentagem)
+ 
+function receberPorcentagemBotao(evento){ 
+    botoesgorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+            
+        if(botao.value === evento.target.value){
+            botao.classList.add("botao-ativo")
+        } 
+    })
+ 
+    porcentagem = parseFloat(evento.target.value) / 100
 }
+ 
